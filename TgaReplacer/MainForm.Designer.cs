8c56f598f5c57a2c29431a0f5298f717372226aa
Kirurobo <kirurobo@gmail.com>
2014-10-11 02:16:19 +0900

    初回コミット

---
+++ TgaReplacer/MainForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace TgaReplacer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelTgaFile = new System.Windows.Forms.Label();
+            this.labelBinaryFile = new System.Windows.Forms.Label();
+            this.buttonSelectTgaFile = new System.Windows.Forms.Button();
+            this.buttonSelectBinaryFile = new System.Windows.Forms.Button();
+            this.textBoxTgaFile = new System.Windows.Forms.TextBox();
+            this.textBoxBinaryFile = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.openFileDialogTargetBinary = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogTga = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.Location = new System.Drawing.Point(155, 56);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(278, 40);
+            this.buttonStart.TabIndex = 11;
+            this.buttonStart.Text = "変換開始";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelTgaFile
+            // 
+            this.labelTgaFile.AutoSize = true;
+            this.labelTgaFile.Location = new System.Drawing.Point(12, 34);
+            this.labelTgaFile.Name = "labelTgaFile";
+            this.labelTgaFile.Size = new System.Drawing.Size(72, 12);
+            this.labelTgaFile.TabIndex = 9;
+            this.labelTgaFile.Text = "使用する画像";
+            // 
+            // labelBinaryFile
+            // 
+            this.labelBinaryFile.AutoSize = true;
+            this.labelBinaryFile.Location = new System.Drawing.Point(12, 9);
+            this.labelBinaryFile.Name = "labelBinaryFile";
+            this.labelBinaryFile.Size = new System.Drawing.Size(76, 12);
+            this.labelBinaryFile.TabIndex = 7;
+            this.labelBinaryFile.Text = "書き換えるDLL";
+            // 
+            // buttonSelectTgaFile
+            // 
+            this.buttonSelectTgaFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSelectTgaFile.Location = new System.Drawing.Point(518, 29);
+            this.buttonSelectTgaFile.Name = "buttonSelectTgaFile";
+            this.buttonSelectTgaFile.Size = new System.Drawing.Size(54, 23);
+            this.buttonSelectTgaFile.TabIndex = 10;
+            this.buttonSelectTgaFile.Text = "参照...";
+            this.buttonSelectTgaFile.UseVisualStyleBackColor = true;
+            this.buttonSelectTgaFile.Click += new System.EventHandler(this.buttonSelectTgaFile_Click);
+            // 
+            // buttonSelectBinaryFile
+            // 
+            this.buttonSelectBinaryFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSelectBinaryFile.Location = new System.Drawing.Point(518, 4);
+            this.buttonSelectBinaryFile.Name = "buttonSelectBinaryFile";
+            this.buttonSelectBinaryFile.Size = new System.Drawing.Size(54, 23);
+            this.buttonSelectBinaryFile.TabIndex = 6;
+            this.buttonSelectBinaryFile.Text = "参照...";
+            this.buttonSelectBinaryFile.UseVisualStyleBackColor = true;
+            this.buttonSelectBinaryFile.Click += new System.EventHandler(this.buttonSelectBinaryFile_Click);
+            // 
+            // textBoxTgaFile
+            // 
+            this.textBoxTgaFile.AllowDrop = true;
+            this.textBoxTgaFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTgaFile.Location = new System.Drawing.Point(90, 31);
+            this.textBoxTgaFile.Name = "textBoxTgaFile";
+            this.textBoxTgaFile.Size = new System.Drawing.Size(421, 19);
+            this.textBoxTgaFile.TabIndex = 8;
+            this.textBoxTgaFile.Text = "healthAndSafety.tga";
+            this.textBoxTgaFile.DragDrop += new System.Windows.Forms.DragEventHandler(this.fileNameBox_DragDrop);
+            this.textBoxTgaFile.DragEnter += new System.Windows.Forms.DragEventHandler(this.fileNameBox_DragEnter);
+            // 
+            // textBoxBinaryFile
+            // 
+            this.textBoxBinaryFile.AllowDrop = true;
+            this.textBoxBinaryFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxBinaryFile.Location = new System.Drawing.Point(90, 6);
+            this.textBoxBinaryFile.Name = "textBoxBinaryFile";
+            this.textBoxBinaryFile.Size = new System.Drawing.Size(422, 19);
+            this.textBoxBinaryFile.TabIndex = 5;
+            this.textBoxBinaryFile.DragDrop += new System.Windows.Forms.DragEventHandler(this.fileNameBox_DragDrop);
+            this.textBoxBinaryFile.DragEnter += new System.Windows.Forms.DragEventHandler(this.fileNameBox_DragEnter);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 107);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(584, 22);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(122, 17);
+            this.toolStripStatusLabel.Text = "ファイルを指定して下さい";
+            this.toolStripStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // openFileDialogTargetBinary
+            // 
+            this.openFileDialogTargetBinary.DefaultExt = "dll";
+            this.openFileDialogTargetBinary.Filter = "DLL|*.dll|All files|*.*";
+            // 
+            // openFileDialogTga
+            // 
+            this.openFileDialogTga.DefaultExt = "tga";
+            this.openFileDialogTga.Filter = "TGA image|*.tga|All files|*.*";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 129);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelTgaFile);
+            this.Controls.Add(this.labelBinaryFile);
+            this.Controls.Add(this.buttonSelectTgaFile);
+            this.Controls.Add(this.buttonSelectBinaryFile);
+            this.Controls.Add(this.textBoxTgaFile);
+            this.Controls.Add(this.textBoxBinaryFile);
+            this.Name = "MainForm";
+            this.Text = "指定ファイル中の1024×512TGA画像を置換するソフト";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelTgaFile;
+        private System.Windows.Forms.Label labelBinaryFile;
+        private System.Windows.Forms.Button buttonSelectTgaFile;
+        private System.Windows.Forms.Button buttonSelectBinaryFile;
+        private System.Windows.Forms.TextBox textBoxTgaFile;
+        private System.Windows.Forms.TextBox textBoxBinaryFile;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialogTargetBinary;
+        private System.Windows.Forms.OpenFileDialog openFileDialogTga;
+
+    }
+}
+
---
+++ TgaReplacer/MainForm.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TgaReplacer
+{
+    public partial class MainForm : Form
+    {
+        private byte[] rawDll;
+        private byte[] rawTga;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void buttonSelectBinaryFile_Click(object sender, EventArgs e)
+        {
+            openFileDialogTargetBinary.FileName = textBoxBinaryFile.Text;
+            DialogResult res = openFileDialogTargetBinary.ShowDialog(this);
+            if (res == DialogResult.OK)
+            {
+                textBoxBinaryFile.Text = openFileDialogTargetBinary.FileName;
+            }
+        }
+
+        private void buttonSelectTgaFile_Click(object sender, EventArgs e)
+        {
+            openFileDialogTga.FileName = textBoxTgaFile.Text;
+            DialogResult res = openFileDialogTga.ShowDialog(this);
+            if (res == DialogResult.OK)
+            {
+                textBoxTgaFile.Text = openFileDialogTga.FileName;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // DLLファイルが空白
+            if (textBoxBinaryFile.Text.Length < 1)
+            {
+                toolStripStatusLabel.Text = "書き換え対象のDLLを指定して下さい";
+                return;
+            }
+
+            // TGAファイルが空白
+            if (textBoxTgaFile.Text.Length < 1)
+            {
+                toolStripStatusLabel.Text = "TGA画像ファイルを指定して下さい";
+                return;
+            }
+
+            // DLLファイルの存在確認
+            if (!File.Exists(textBoxBinaryFile.Text))
+            {
+               toolStripStatusLabel.Text = "書き換え対象ファイルが存在しません";
+                return;
+            }
+
+            // TGAファイルの存在確認
+            if (!File.Exists(textBoxTgaFile.Text))
+            {
+                toolStripStatusLabel.Text = "画像ファイルが存在しません";
+                return;
+            }
+
+            // 処理前の読込
+            if (LoadBinaryFile())
+            {
+                // 終了メッセージ
+                toolStripStatusLabel.Text = "バイナリファイル読込完了";
+
+                if (LoadTgaFile())
+                {
+                    if (Convert())
+                    {
+                        // 読込みと変換の終了メッセージはConvert()中で表示される
+
+                        if (Save())
+                        {
+                            // 保存終了メッセージ
+                            toolStripStatusLabel.Text = "保存しました";
+                        }
+                        else
+                        {
+                            // 保存時エラー
+                            toolStripStatusLabel.Text = "保存されませんでした";
+                        }
+                    }
+                    else
+                    {
+                        // 変換時のエラーは Convert() 中で表示される
+                    }
+                }
+                else
+                {
+                    // 終了メッセージ
+                    toolStripStatusLabel.Text = "画像ファイル読込エラー";
+                }
+            }
+            else
+            {
+                // 終了メッセージ
+                toolStripStatusLabel.Text = "バイナリファイル読込エラー";
+            }
+        }
+
+        /// <summary>
+        /// 変換処理。失敗したときはデータは保証しない。
+        /// </summary>
+        /// <returns>成功すればtrue</returns>
+        private bool Convert()
+        {
+            if (rawDll == null || rawTga == null) return false;
+
+            int convertedCount = 0;
+            int start = 0;
+            int end = -1;
+            while (true) {
+                start = FindHeader(end + 1);
+                if (start < 0)
+                {
+                    break;
+                }
+
+                end = FindFooter(start + 1);
+                if (end < 0)
+                {
+                    toolStripStatusLabel.Text = "バイナリファイル中でTGA画像の終端が見つかりません";
+                    return false;
+                }
+
+                if ((end - start + 1) < rawTga.Length)
+                {
+                    toolStripStatusLabel.Text = "TGA画像の容量が大きすぎます";
+                    return false;
+                }
+
+                // 差し替え処理
+                int index = start;
+                foreach (byte dat in rawTga)
+                {
+                    rawDll[index] = dat;
+                    index++;
+                }
+                convertedCount++;
+            }
+
+            // 一つも画像が見つからなかった
+            if (convertedCount < 1)
+            {
+                toolStripStatusLabel.Text = "バイナリファイル中で対応するTGA画像が見つかりません";
+                return false;
+            }
+
+            // 変換数を表示
+            toolStripStatusLabel.Text = convertedCount.ToString() + "件の画像を変換しました";
+
+            return true;
+        }
+
+        /// <summary>
+        /// 変換後のバイナリファイルを保存
+        /// </summary>
+        /// <returns></returns>
+        private bool Save()
+        {
+            if (rawDll == null) return false;
+
+            string path = textBoxBinaryFile.Text;
+            string backupPath = path + ".bak";
+            string tmpPath = path + ".tmp";
+
+            // バックアップファイルが既存ならばバックアップしない
+            if (File.Exists(backupPath))
+            {
+                DialogResult result = MessageBox.Show("バックアップが存在するため、新たにバックアップは作成しません" + Environment.NewLine + "本当に書き換えますか？", "確認", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Cancel)
+                {
+                    return false;
+                }
+                backupPath = null;  // バックアップが既存ならばバックアップを作成しない
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("本当に書き換えますか？" + Environment.NewLine + "元のファイルは .bak として保存されます", "確認", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Cancel)
+                {
+                    return false;
+                }
+            }
+
+            // 一時ファイルに保存
+            File.WriteAllBytes(tmpPath, rawDll);
+
+            // 一時ファイルを元のファイルと置き換え
+            File.Replace(tmpPath, path, backupPath);
+
+            return true;
+        }
+
+        /// <summary>
+        /// 書き換え対象のバイナリファイルを読み込み、画像があれば表示
+        /// </summary>
+        /// <returns></returns>
+        private bool LoadBinaryFile()
+        {
+            rawDll = null;
+
+            try
+            {
+                rawDll = File.ReadAllBytes(textBoxBinaryFile.Text);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 差し替えする画像ファイルを読み込み
+        /// </summary>
+        /// <returns></returns>
+        private bool LoadTgaFile()
+        {
+            try
+            {
+                rawTga = File.ReadAllBytes(textBoxTgaFile.Text);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 1024×512 TGA画像のヘッダーらしき並びを探す。
+        /// </summary>
+        /// <param name="startIndex">検索を開始するバイト位置</param>
+        /// <returns>見つかった位置。見つからなければ -1</returns>
+        private int FindHeader(int startIndex)
+        {
+            if (rawDll == null) return -1;
+
+            // ヘッダー。これと見比べる。ただし3文字目は10(0x0A)でも通す。
+            byte[] header = { 0, 0, 2, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 4, 0, 2,  32};
+
+            for (int currentIndex = startIndex; currentIndex <= (rawDll.Length - header.Length); currentIndex++)
+            {
+                bool unmatch = false;
+                for (int headerIndex = 0; headerIndex < header.Length; headerIndex++)
+                {
+                    byte dat = rawDll[currentIndex + headerIndex];
+                    if (dat != header[headerIndex] && (headerIndex != 2 || dat != 0x0A))
+                    {
+                        unmatch = true;
+                        break;
+                    }
+                }
+                if (!unmatch) return currentIndex;    // 見つかった
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// TGA画像のフッターを探して最後のインデックスを返す。
+        /// </summary>
+        /// <param name="startIndex">検索を開始するバイト位置</param>
+        /// <returns>見つかった範囲の終端位置。見つからなければ -1</returns>
+        private int FindFooter(int startIndex)
+        {
+            if (rawDll == null) return -1;
+
+            // フッター。これと見比べる。
+            string footerString = "TRUEVISION-XFILE.\0";
+            byte[] footer = System.Text.Encoding.ASCII.GetBytes(footerString);
+
+            for (int currentIndex = startIndex; currentIndex <= (rawDll.Length - footer.Length); currentIndex++)
+            {
+                bool unmatch = false;
+                for (int footerIndex = 0; footerIndex < footer.Length; footerIndex++)
+                {
+                    byte dat = rawDll[currentIndex + footerIndex];
+                    if (dat != footer[footerIndex])
+                    {
+                        unmatch = true;
+                        break;
+                    }
+                }
+                if (!unmatch) return (currentIndex + footer.Length - 1);    // 見つかった
+            }
+            return -1;
+        }
+
+
+        /// <summary>
+        /// ファイルをドラッグされてきたときはマウスカーソルを替える
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fileNameBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// ファイルをテキストボックスにドロップされた場合はそのパスを入れる
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fileNameBox_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (sender is TextBox)
+            {
+                ((TextBox)sender).Text = files[0];
+            }
+        }
+    }
+}
